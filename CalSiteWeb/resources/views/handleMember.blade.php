f2d987a14caceff2e0de10b81774d977c16eb2ff
Karim Luy <karim.luy@he-arc.ch>
2016-12-18 18:55:52 +0100

    made the form less vomitable

--- CalSiteWeb/resources/views/handleMember.blade.php
+++ CalSiteWeb/resources/views/handleMember.blade.php
@@ -3,16 +3,19 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
+            <div class="col-md-12">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                     </div>
                     <div class="panel-body">
-                        <form class="form-horizontal" role="form" method="POST" action="{{url('calendar/'.$agenda->id.'/members/')}}">
+                        <label class="col-md-12 control-label">
+                            Add to this calendar's users:
+                        </label>
+                        <form class="row form-horizontal" role="form" method="POST" action="{{url('calendar/'.$agenda->id.'/members/')}}">
                             {{ csrf_field() }}
 
-                            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                                <label for="email" class="col-md-4 control-label">E-Mail Address Members</label>
+                            <div class="col-md-12 form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                                <label for="email" class="col-md-3 control-label">E-Mail Address Members</label>
 
                                 <div class="col-md-6">
                                     <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
@@ -24,75 +27,77 @@
                                     @endif
                                 </div>
                             </div>
-                            <div class="form-group">
-                                <label for="permission" class="col-md-4 control-label">Permissions</label>
+                            <div class="row form-group">
+                                <label for="permission" class="col-md-3 control-label">Permissions</label>
 
-                                <div class="col-md-6">
-                                    <table name="permission">
-                                        <tr>
-                                            <td>Add task</td>
-                                            <td>Edit task</td>
-                                            <td>Delete task</td>
-                                            <td>Edit members</td>
-                                            <td>Edit calendar</td>
-                                            <td>Delete calendar</td>
-                                            <td></td>
-                                            <td></td>
-                                        </tr>
-                                        <tr>
-                                            <td><input type="checkbox" name="add_task" ></td>
-                                            <td><input type="checkbox" name="edit_task"></td>
-                                            <td><input type="checkbox" name="delete_task"></td>
-                                            <td><input type="checkbox" name="edit_member"></td>
-                                            <td><input type="checkbox" name="edit_calendar"></td>
-                                            <td><input type="checkbox" name="delete_calendar"></td>
-                                            <td><button type="submit" class="btn">Save</button></td>
-                                            <td><button type="submit" class="btn">Remove</button></td>
-
-                                        </tr>
-                                    </table>
+                                <div class="col-md-9">
+                                    <div class="row">
+                                        <div class="col-md-1">
+                                            <p>Add task</p>
+                                            <input type="checkbox" name="add_task">
+                                        </div>
+                                        <div class="col-md-1">
+                                            <p>Edit task</p>
+                                            <input type="checkbox" name="edit_task">
+                                        </div>
+                                        <div class="col-md-1">
+                                            <p>Delete task</p>
+                                            <input type="checkbox" name="delete_task">
+                                        </div>
+                                        <div class="col-md-1">
+                                            <p>Edit members</p>
+                                            <input type="checkbox" name="edit_member">
+                                        </div>
+                                        <div class="col-md-1">
+                                            <p>Edit calendar</p>
+                                            <input type="checkbox" name="edit_calendar">
+                                        </div>
+                                        <div class="col-md-1">
+                                            <p>Delete calendar</p>
+                                            <input type="checkbox" name="delete_calendar">
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
 
-                            <div class="form-group">
-                                <div class="col-md-8 col-md-offset-6">
-                                    <button type="submit" class="btn btn-primary">
+                            <div class="row form-group">
+                                <div class="col-md-offset-5">
+                                    <button type="submit" value="add" class="btn btn-primary">
                                         Register Member
                                     </button>
                                 </div>
                             </div>
                         </form>
-                        <table>
-                            <tr>
-                                <td>Email</td>
-                                <td>Add task</td>
-                                <td>Edit task</td>
-                                <td>Delete task</td>
-                                <td>Edit members</td>
-                                <td>Edit calendar</td>
-                                <td>Delete calendar</td>
-                                <td></td>
-                                <td></td>
-                            </tr>
+                        <div class="row form-group">
+                            <label class="col-md-12 control-label">
+                                Edit this calendar's users:
+                            </label>
+                            <div class="col-md-12 col-md-offset-1">
+                                <label class="col-md-2 control-label">Email</label>
+                                <label class="col-md-1 control-label">Add task</label>
+                                <label class="col-md-1 control-label">Edit task</label>
+                                <label class="col-md-1 control-label">Delete task</label>
+                                <label class="col-md-1 control-label">Edit members</label>
+                                <label class="col-md-1 control-label">Edit calendar</label>
+                                <label class="col-md-1 control-label">Delete calendar</label>
+                            </div>
                             @foreach($users as $user)
-
-                                <form class="form-horizontal" role="form" method="POST" action="{{url('calendar/'.$agenda->id.'/members/')}}">
-                                <tr>
-                                    <input type="hidden" name="user_id" value="$user->id"/>
-                                    <td>{{ $user->email }}</td>
-                                    <td><input type="checkbox" name="add_task" {{ $user->pivot->add_task? 'checked' : '' }}></td>
-                                    <td><input type="checkbox" name="edit_task" {{ $user->pivot->edit_task? 'checked' : '' }}></td>
-                                    <td><input type="checkbox" name="delete_task" {{ $user->pivot->delete_task? 'checked' : '' }}></td>
-                                    <td><input type="checkbox" name="edit_member" {{ $user->pivot->edit_member? 'checked' : '' }}></td>
-                                    <td><input type="checkbox" name="edit_calendar" {{ $user->pivot->edit_calendar? 'checked' : '' }}></td>
-                                    <td><input type="checkbox" name="delete_calendar" {{ $user->pivot->delete_calendar? 'checked' : '' }}></td>
-                                    <td><button type="submit" class="btn">Save</button></td>
-                                    <td><button type="submit" class="btn">Remove</button></td>
-
-                                </tr>
-                                </form>
+                                <div class="col-md-12 col-md-offset-1">
+                                    <form class="form-horizontal" role="form" method="POST" action="{{url('calendar/'.$agenda->id.'/members/')}}">
+                                    <input type="hidden" name="user_id" value="{{$user->id}}"/>
+                                        <div class="col-md-2">{{ $user->email }}</div>
+                                        <div class="col-md-1"><input type="checkbox" name="add_task" {{ $user->pivot->add_task? 'checked' : '' }}></div>
+                                        <div class="col-md-1"><input type="checkbox" name="edit_task" {{ $user->pivot->edit_task? 'checked' : '' }}></div>
+                                        <div class="col-md-1"><input type="checkbox" name="delete_task" {{ $user->pivot->delete_task? 'checked' : '' }}></div>
+                                        <div class="col-md-1"><input type="checkbox" name="edit_member" {{ $user->pivot->edit_member? 'checked' : '' }}></div>
+                                        <div class="col-md-1"><input type="checkbox" name="edit_calendar" {{ $user->pivot->edit_calendar? 'checked' : '' }}></div>
+                                        <div class="col-md-1"><input type="checkbox" name="delete_calendar" {{ $user->pivot->delete_calendar? 'checked' : '' }}></div>
+                                        <div class="col-md-1"><button type="submit" class="btn btn-default">Save</button></div>
+                                        <div class="col-md-1"><button type="submit" class="btn btn-danger">Remove</button></div>
+                                    </form>
+                                </div>
                             @endforeach
-                        </table>
+                        </div>
                     </div>
                 </div>
             </div>
